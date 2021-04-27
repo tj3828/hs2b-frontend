ef13350ab2fa52525a6e56b44406f4c4ae3b325b
touch-bell <dhkrwhd1!>
2021-04-28 03:02:24 +0900

    axios 추가 / 백엔드 연동 확인

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  useEffect(() => {
+    axios.get('/api/test')
+      .then((response) => {
+        console.log(response)
+        setLists(response.data)
+      })
+  }, [])
+
+  const [lists, setLists] = useState([])
+  const [value, setValue] = useState("")
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value)
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post('/api/value', null, {params:{value:value}})
+      .then(response => {
+        console.log(response)
+        if(response.status == 200) {
+          setLists([...lists, response.data])
+          setValue("")
+        } else {
+          alert('값을 DB에 넣는데 실패했습니다.')
+        }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        개발환경 - nginx, react 연결
+        <div className="contatiner">
+            {lists && lists.map((list, index) => (
+              <li key={index}>{list.value}</li>
+            ))}
+          <br/>
+          <form className="example" onSubmit={submitHandler}>
+            <input type="text" placeholder="입력해주세요..." onChange={changeHandler} value={value}/>
+            <button type="submit">확인</button>
+          </form>
+        </div>
       </header>
     </div>
   );
