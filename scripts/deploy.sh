1389a422f9a091eb4d681e4559f1a949c1f9af7d
touch-bell <dhkrwhd1!>
2021-05-01 15:31:09 +0900

    ec2 react deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "> hs2b-frontend 배포"
-sudo cp -rf /home/ec2-user/app/frontend/build /usr/share/nginx/html
+sudo cp -rf /home/ec2-user/app/hs2b/frontend/build/* /usr/share/nginx/html
